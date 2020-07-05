26a21e54cfc3eccae9f6c377cd42e0c0f01b0d02
Даниил Стефаненков <daniilstef2391@gmail.com>
2020-07-05 18:03:10 +0300

    Добавьте файлы проекта.

---
+++ pract10_Form/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace pract10_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submit = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // submit
+            // 
+            this.submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.submit.Location = new System.Drawing.Point(242, 260);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(139, 37);
+            this.submit.TabIndex = 0;
+            this.submit.Text = "Вывести";
+            this.submit.UseVisualStyleBackColor = true;
+            // 
+            // output
+            // 
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.output.Location = new System.Drawing.Point(12, 12);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(604, 227);
+            this.output.TabIndex = 1;
+            this.output.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 309);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.submit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.RichTextBox output;
+    }
+}
+
---
+++ pract10_Form/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace pract10_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            submit.Click += Submit_Click;
+        }
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists("C:\\temp"))
+            {
+                if (Directory.GetDirectories("C:\\temp").Length == 0)
+                {
+                    output.Text = "";
+                    Directory.CreateDirectory("C:\\temp\\К1");
+                    Directory.CreateDirectory("C:\\temp\\К2");
+                    File.Create("C:\\temp\\К1\\t1.txt").Close();
+                    File.Create("C:\\temp\\К1\\t2.txt").Close();
+                    File.WriteAllText("C:\\temp\\К1\\t1.txt", "Иванов Иван Иванович, 1965 года рождения, место жительства г. Саратов");
+                    File.WriteAllText("C:\\temp\\К1\\t2.txt", "Петров Сергей Федорович, 1966 года рождения, место жительства г.Энгельс");
+                    File.Create("C:\\temp\\К2\\t3.txt").Close();
+                    File.WriteAllText("C:\\temp\\К2\\t3.txt", File.ReadAllText("C:\\temp\\К1\\t1.txt") + "\n" + File.ReadAllText("C:\\temp\\К1\\t2.txt"));
+                    FileInfo t1 = new FileInfo("C:\\temp\\К1\\t1.txt");
+                    output.Text += "Полное имя: " + t1.FullName + ", Размер: " + t1.Length + ", Последнее время записи: " + t1.LastWriteTime + "\n";
+                    FileInfo t2 = new FileInfo("C:\\temp\\К1\\t2.txt");
+                    output.Text += "Полное имя: " + t2.FullName + ", Размер: " + t2.Length + ", Последнее время записи: " + t2.LastWriteTime + "\n";
+                    FileInfo t3 = new FileInfo("C:\\temp\\К2\\t3.txt");
+                    output.Text += "Полное имя: " + t3.FullName + ", Размер: " + t3.Length + ", Последнее время записи: " + t3.LastWriteTime + "\n";
+                    t2.MoveTo("C:\\temp\\К2\\t2.txt");
+                    t1.CopyTo("C:\\temp\\К2\\t1.txt");
+                    Directory.Move("C:\\temp\\К2", "C:\\temp\\All");
+                    Directory.Delete("C:\\temp\\К1", true);
+                    string[] f = Directory.GetFiles("C:\\temp\\All");
+                    output.Text += "\nФайлы папки All:\n";
+                    foreach (string item in f)
+                    {
+                        FileInfo t = new FileInfo(item);
+                        output.Text += "Полное имя: " + t.FullName + ", Размер: " + t.Length + ", Последнее время записи: " + t.LastWriteTime + "\n";
+                    }
+                }
+                else
+                {
+                    output.Text = "Папка temp не пуста";
+                }
+            }
+            else
+            {
+                output.Text = "Папки temp не существует";
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
